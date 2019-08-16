a3da95eb370a629114cd538fda0e3306dd209178
Vuong Nguyen <vuongnd90@gmail.com>
2019-08-16 18:38:48 +0900

    add controller and model for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  
+  get  '/about',    to: 'static_pages#about'
+  get  '/home',     to: 'static_pages#home'
+
+  get  '/signup',   to: 'users#new'
+  post '/signup',   to: 'users#create'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
